2d36e9954528da8cbd116ce808359469646f930f
tornadofsoulz <tornadofsoulz@yandex.ru>
2023-05-14 23:41:31 +0300

    init

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/health-check' && req.method === 'GET') {
+  if (req.url == '/') { //check the URL of the current request
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+  }
+  else if (req.url === '/health-check' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Server is healthy' }));
   } else {
